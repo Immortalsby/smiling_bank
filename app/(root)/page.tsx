7cda8ee9c2f722d60ef2564e1957c838dc56e564
Immortalsby <sby945913@gmail.com>
2024-06-12 22:25:21 +0200

    Add sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn = { firstName: 'Boyuan', lastName: 'SHI', email: 'contact@boyuanshi.com' }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="开心搞笑的管理你的账单"
+            />
+
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={1250.35}
+              />
+        </header>
+
+        RECENT 
+      </div>
+      <RightSidebar
+      user={loggedIn}
+      trasactions={[]}
+      banks={[{ currentBalance:123.50 }, {currentBalance:500}]}
+      />
+    </section>
   )
 }
 
